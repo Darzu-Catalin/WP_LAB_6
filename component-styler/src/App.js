d65b60eb12fea2773e8ddc168df2c68a49cc355c
Darzu Catalin <cs.darzu@gmail.com>
2026-05-03 13:42:01 +0300

    Initial React app setup with project structure

---
+++ component-styler/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1 className="app-title">🎨 Component Styler</h1>
+        <p>Visual component styling and composition tool</p>
+      </header>
+      <div className="app-workspace">
+        <p>Coming soon: drag-and-drop component canvas</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
